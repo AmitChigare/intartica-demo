e9b9598a52d75c5d3dedaf6095fd5edba64f5fa3
AmitChigare <amit.gokakfalls17@gmail.com>
2023-12-21 23:08:45 +0530

    Added builder page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./pages/about/About";
 import Services from "./pages/services/Services";
+import Builder from "./pages/builder/Builder";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/builder" element={<Builder />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/builder/Builder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./builder.css";
+
+const Builder = () => {
+  return (
+    <div className="container demo">
+      All the builder code here <p>hi</p>
+    </div>
+  );
+};
+
+export default Builder;
